a59a033582c898b641df0950ab601a5262e5585e
Zorzamoth <zorzamoth@gmail.com>
2026-01-31 21:49:40 +0100

    main menu & credits screens changes
    - Cambios en la lógica de la pantalla de inicio, ahora los botones hacen cada uno su función.
    - Ahora se usa la tipografía retro en la pantalla de inicio y los créditos.

--- ux/main_menu.gd
+++ ux/main_menu.gd
@@ -8,3 +8,13 @@ func _on_btn_play_pressed() -> void:
 	await transition_screen.end_transition_signal
 	
 	get_tree().change_scene_to_file("res://testing_level.tscn")
+	
+
+
+func _on_btn_credits_pressed() -> void:
+	
+	get_tree().change_scene_to_file("res://ux/credits_menu.tscn")
+
+
+func _on_btn_quit_pressed() -> void:
+	get_tree().quit()
