094c9ab26349d016f3756f83048355c1404b207a
Manuel Otero <manuelOtero.af@gmail.com>
2026-02-01 12:39:00 +0100

    FIX warnnings

--- interactors/semaforo.gd
+++ interactors/semaforo.gd
@@ -1,4 +1,5 @@
 extends "res://interactors/interactor_base.gd".InteractorBase
+
 @onready var semaforo: Sprite2D = $semaforo
 @onready var path_2d: Path2D = $Path2D
 
@@ -8,10 +9,10 @@ func on_active_nube_interact(power:Nube.Powers):
 		for child in path_2d.get_children():
 			child.set_stop_in_next_loop()
 	
-func on_nube_interact(power:Nube.Powers):
+func on_nube_interact(_power:Nube.Powers):
 	pass
 	
-func is_manolo_alive(manolo_pos:Vector2):
+func is_manolo_alive(_manolo_pos:Vector2):
 	return false
 	
 func get_death_text():
